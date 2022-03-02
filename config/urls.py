a72e7c104d9f9f9916addca2fbe39c0ee5bbb9ef
James Spadafora <spadimationvfx@gmail.com>
2022-03-02 14:31:34 -0800

    Update admin URL

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("casper/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/v1/", include("pages.urls")),
 ]
 
